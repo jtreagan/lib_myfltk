27b1eccc39199d4057c5e0810839777f6ef7ac86
John T. Reagan <john@ramblingreagans.com>
2024-10-02 11:27:22 -0700

    Just in case...

--- src/lib.rs
+++ src/lib.rs
@@ -59,6 +59,58 @@ pub mod fltkutils {
         retvec
     }
 
+    pub fn radio_lightbtn_menu(flist: &Vec<String>) -> Vec<String> {
+        let newvec: RefCell<Vec<String>> = RefCell::new(Vec::new());
+        let keepers: Rc<RefCell<Vec<String>>> = Rc::new(newvec);
+
+        let app = app::App::default();
+        let mut win = window::Window::default().with_size(400, 300);
+        let mut row = group::Flex::default_fill().row();
+        let scroll = group::Scroll::default();
+        row.fixed(&scroll, 150);
+        let pack = group::Pack::default().with_size(100, 300);
+
+        for file in flist {
+            let _check = button::RadioLightButton::default()
+                .with_label(file)
+                .with_size(0, 30);
+        }
+
+        pack.end();
+        scroll.end();
+
+        let mut btn = button::Button::default().with_label("@>");
+        row.fixed(&btn, 30);
+        let mut output = output::MultilineOutput::default();
+
+        row.end();
+        win.end();
+        win.show();
+
+        let keepers_clone = Rc::clone(&keepers);
+        btn.set_callback(move |_b| {
+            output.set_value("");
+            let mut string = String::new();
+            for i in 0..pack.children() {
+                let radio: button::RadioLightButton = button::RadioLightButton::
+                from_dyn_widget(&pack.child(i).unwrap()).unwrap();
+                if radio.is_toggled() {
+                    string.push_str(&radio.label());
+                    string.push('\n');
+                    keepers_clone.borrow_mut().push(radio.label().clone());
+                }
+            }
+            output.set_value(&string);
+        });
+
+        app.run().unwrap();
+
+        let retvec: Vec<String> = take(&mut keepers.borrow_mut());
+        retvec
+    }
+
+
+    /*  First attempt at single-item return
 
     pub fn radio_lightbtn_menu(list: &Vec<String>)  {
         //let newvec: RefCell<Vec<String>> = RefCell::new(Vec::new());
@@ -70,22 +122,18 @@ pub mod fltkutils {
         let scroll = group::Scroll::default();
         let mut pack = group::Pack::default().with_size(200,300);
 
-        let mut radlite = button::RadioLightButton::default();
         for fname in list {
-            let radlite = button::RadioLightButton::default()
+            let _radlite = button::RadioLightButton::default()
                 .with_label(fname)
                 .with_size(75, 50);
-            pack.add(&radlite);
         }
 
         pack.end();
         scroll.end();
 
-        //flex.fixed(&pack, 50);
-
         // Sets up the  Submit  button
-        let mut btn = button::Button::default().with_label("Submit");
-        flex.fixed(&btn, 30);
+        let mut btn = button::Button::default().with_label("@Submit");
+        flex.fixed(&btn, 50);
         //let mut output = output::MultilineOutput::default();
 
         flex.end();
@@ -117,6 +165,7 @@ pub mod fltkutils {
         //retvec
     }
 
+     */
 
 
 }  // --------- End   fltkutils   module ----------
