33ae330cef0905fbc056913cd3e2f71fc03ca89a
John T. Reagan <john@ramblingreagans.com>
2024-10-02 10:05:01 -0700

    Well, got the file menu working right.  Still need to get the submit button correct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-
+#![warn(unused_imports)]
 
 
 pub mod fltkutils {
@@ -7,6 +7,7 @@ pub mod fltkutils {
     use std::rc::Rc;
     use fltk::{app, button, group, output, window};
     use fltk::prelude::{GroupExt, InputExt, WidgetBase, WidgetExt};
+    //use lib_jt::vec::*;
 
     pub fn chkbox_shift_menu(flist: &Vec<String>) -> Vec<String> {
         let newvec: RefCell<Vec<String>> = RefCell::new(Vec::new());
@@ -57,4 +58,65 @@ pub mod fltkutils {
         let retvec: Vec<String> = take(&mut keepers.borrow_mut());
         retvec
     }
-}
+
+
+    pub fn radio_lightbtn_menu(list: &Vec<String>)  {
+        //let newvec: RefCell<Vec<String>> = RefCell::new(Vec::new());
+        //let keepers: Rc<RefCell<Vec<String>>> = Rc::new(newvec);
+
+        let app = app::App::default();
+        let mut win = window::Window::default().with_size(200, 300);
+        let mut flex = group::Flex::default_fill();
+        let scroll = group::Scroll::default();
+        let mut pack = group::Pack::default().with_size(200,300);
+
+        let mut radlite = button::RadioLightButton::default();
+        for fname in list {
+            let radlite = button::RadioLightButton::default()
+                .with_label(fname)
+                .with_size(75, 50);
+            pack.add(&radlite);
+        }
+
+        pack.end();
+        scroll.end();
+
+        //flex.fixed(&pack, 50);
+
+        // Sets up the  Submit  button
+        let mut btn = button::Button::default().with_label("Submit");
+        flex.fixed(&btn, 30);
+        //let mut output = output::MultilineOutput::default();
+
+        flex.end();
+        win.end();
+        win.show();
+
+        /*
+        let keepers_clone = Rc::clone(&keepers);
+        btn.set_callback(move |_b| {
+            output.set_value("");
+            let mut string = String::new();
+            for i in 0..pack.children() {
+                let check: button::CheckButton = button::CheckButton::
+                from_dyn_widget(&pack.child(i).unwrap()).unwrap();
+                if check.is_checked() {
+                    string.push_str(&check.label());
+                    string.push('\n');
+                    keepers_clone.borrow_mut().push(check.label().clone());
+                }
+            }
+            output.set_value(&string);
+        });
+
+         */
+
+        app.run().unwrap();
+
+        //let retvec: Vec<String> = take(&mut keepers.borrow_mut());
+        //retvec
+    }
+
+
+
+}  // --------- End   fltkutils   module ----------
--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,19 @@
 
 use lib_file::file_fltk::file_pathonly;
 use lib_file::file_mngmnt::file_get_dir_list;
-use lib_myfltk::fltkutils::chkbox_shift_menu;
+use lib_myfltk::fltkutils::{chkbox_shift_menu, radio_lightbtn_menu};
 
 fn main() {
 
     let path = file_pathonly();
-    println!("The chosen path is:  {} \n", path);
+    //println!("The chosen path is:  {} \n", path);
 
     let flist = file_get_dir_list(&path);
-    println!("The files in that directory are:  {:?} \n", flist);
+    //println!("The files in that directory are:  {:?} \n", flist);
     
-    let fnamesvctr = chkbox_shift_menu( &flist);
+    radio_lightbtn_menu( &flist);
 
-    println!("The vector of chosen file names is:  {:?} \n", fnamesvctr);
+    //println!("The vector of chosen file names is:  {:?} \n", fnamesvctr);
 
 
 }
